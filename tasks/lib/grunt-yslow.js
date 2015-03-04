54bcf61374f6391d91d080f0f875a2e38b1d2619
Albert Fernández <albertfdp@gmail.com>
2015-03-04 14:14:50 +0100

    Pass phantomJS configuration parameters as well

--- tasks/lib/grunt-yslow.js
+++ tasks/lib/grunt-yslow.js
@@ -4,8 +4,13 @@ exports.init = function(grunt) {
   exports.yslow = function(url, report, options, callback) {
 
     var pathToYSlow = path.resolve('./node_modules/grunt-yslow-test/tasks/lib/yslow.js'),
-        command = 'phantomjs '+pathToYSlow,
+        phantomJsCommand = 'phantomjs',
+        command = pathToYSlow,
         exec = require('child_process').exec;
+
+    if (options.phantomjs.ignore_ssl_errors) {
+      phantomJsCommand += ' --ignore_ssl_errors=' + options.phantomjs.ignore_ssl_errors;
+    }
 
     // Add options documented in the following web site:
     //   http://yslow.org/phantomjs/
@@ -38,18 +43,19 @@ exports.init = function(grunt) {
     }
 
     command += " " + url;
+    command = phantomJsCommand + command;
     grunt.log.write("Command: " + command);
 
     function puts(error, stdout, stderr) {
       grunt.log.write('\nRunning YSlow on "' + url + '":\n');
-      
+
       if (report) {
         grunt.log.write("Saving report to "+report);
         grunt.file.write(report, stdout);
       } else {
         grunt.log.write(stdout);
       }
-      
+
       if ( error !== null ) {
         callback(error);
       } else {
@@ -58,8 +64,8 @@ exports.init = function(grunt) {
     }
 
     exec(command, puts);
-    
+
   };
-  
+
   return exports;
 };
